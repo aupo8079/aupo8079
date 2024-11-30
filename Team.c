d3e1231c1d96e218be7db4f2d541221beb4bd6ef
aupo8079 <archanaupo@gmail.com>
2024-11-30 22:09:09 +0600

    Add files via upload

---
+++ Team.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    int ara[10][3];
+    int count = 0,total = 0;
+
+        
+    scanf("%d",&n);
+        
+    for(i = 0;i <  n;i++) {
+        for(j = 0;j < 3;j++) {
+            scanf("%d",&ara[i][j]);
+        }
+    }
+    for(i = 0;i < n;i++) {
+        for(j = 0;j < 3;j++) {
+            if(ara[i][j] == 1) {
+                count+=1;
+                
+            }
+         else {
+                ara[i][j] = ara[i][j];
+            }    
+            
+        }
+        if(count >= 2) {
+            total += 1;
+        }
+        count = 0;
+    }
+    printf("%d\n",total);
+    
+}
