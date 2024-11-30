3f5321ea94c2428fb03ff627005dd40ed206344e
aupo8079 <archanaupo@gmail.com>
2024-11-30 23:24:41 +0600

    Update Team.c

--- Team.c
+++ Team.c
@@ -2,11 +2,12 @@
 int main()
 {
     int i,j,n;
-    int ara[10][3];
+    int ara[1000][3];
     int count = 0,total = 0;    
     scanf("%d",&n);
-        
-    for(i = 0;i <  n;i++) {
+
+    if (n >= 1 && n <= 1000) {
+         for(i = 0;i <  n;i++) {
         for(j = 0;j < 3;j++) {
             scanf("%d",&ara[i][j]);
         }
@@ -27,4 +28,6 @@ int main()
     }
     printf("%d\n",total);
     
+    }
+   
 }
