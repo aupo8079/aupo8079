97f6c8f332444ef59b1c52f48ad76adf9618096a
Aupo <archanaupo@gmail.com>
2025-01-04 23:14:20 +0600

    Update Team.c

--- Team.c
+++ Team.c
@@ -6,20 +6,13 @@ int main()
     scanf("%d",&n);
     int ara[n][3];
 
-    if (n >= 1 && n <= 1000) {
-         for(i = 0;i <  n;i++) {
-        for(j = 0;j < 3;j++) {
-            scanf("%d",&ara[i][j]);
-        }
-    }
+   
     for(i = 0;i < n;i++) {
         for(j = 0;j < 3;j++) {
+            scanf("%d",&ara[i][j]);
             if(ara[i][j] == 1) {
-                count+=1;
-                
-            }
-          
-            
+                count+=1;    
+            }     
         }
         if(count >= 2) {
             total += 1;
@@ -27,7 +20,5 @@ int main()
         count = 0;
     }
     printf("%d\n",total);
-    
-    }
-   
 }
+
